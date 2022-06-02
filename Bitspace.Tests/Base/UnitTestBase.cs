0a1f798527ecaf81d9bf1aaf7c436006906589e0
Bryce <techbryce@hotmail.com>
2022-06-02 22:42:30 +1000

    UPDATE - Minor configuration changes.

---
+++ Bitspace.Tests/Base/UnitTestBase.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using Moq.AutoMock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitspace.Tests.Base
+{
+    public class UnitTestBase<T> where T : class
+    {
+        public UnitTestBase()
+        {
+            Mocker = new AutoMocker();
+            Sut = Mocker.CreateInstance<T>();
+        }
+
+        protected T Sut { get; set; }
+        protected AutoMocker Mocker { get; set; }
+    }
+}
