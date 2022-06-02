0ab1297a96589c38b1042aeddd53a3b5bf52ea2c
Bryce <techbryce@hotmail.com>
2022-06-02 22:40:47 +1000

    UPDATE - Added NavigationConstants.cs

---
+++ Bitspace/Bitspace/Constants/NavigationConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bitspace.Constants
+{
+    public static class NavigationConstants
+    {
+        public static string Mainpage = nameof(Mainpage);
+    }
+}
