6ebc5663f4fd8e0bb7a2ed3fad1b188ffa40964f
Bryce <techbryce@hotmail.com>
2022-06-02 22:40:29 +1000

    UPDATE - Added platform properties to DeviceInformationService

---
+++ Bitspace/Bitspace/Services/IDeviceInformationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bitspace.Services
+{
+    public interface IDeviceInformationService
+    {
+        bool IsiOS { get; }
+        bool IsAndroid { get; }
+    }
+}
